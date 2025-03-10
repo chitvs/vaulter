28185eaf4028d943a7367da2d4c67e93c213ba11
Alessandro <alessandro.chitarrini51@gmail.com>
2025-03-10 16:25:59 +0100

    add functions

--- main.ts
+++ main.ts
@@ -1,133 +1,104 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface VaultStatsPluginSettings {
-	mySetting: string;
+interface VaulterPluginSettings {
+  showInStatusBar: boolean;  // Setting to control status bar visibility
+  mySetting: string;
 }
 
-const DEFAULT_SETTINGS: VaultStatsPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: VaulterPluginSettings = {
+  showInStatusBar: true,  // Default to show in status bar
+  mySetting: 'default'
 }
 
-export default class VaultStatsPlugin extends Plugin {
-	settings: VaultStatsPluginSettings;
+export default class VaulterPlugin extends Plugin {
+  settings: VaulterPluginSettings;
+  statusBarItemEl: HTMLElement;
 
-	async onload() {
-		await this.loadSettings();
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('Hello World');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+  async onload() {
+    await this.loadSettings();
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+    this.statusBarItemEl = this.addStatusBarItem();
+    this.updateVaulter();
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    // Register an event listener for file changes to update the stats dynamically
+    this.registerEvent(this.app.vault.on('create', this.updateVaulter.bind(this)));
+    this.registerEvent(this.app.vault.on('delete', this.updateVaulter.bind(this)));
+    this.registerEvent(this.app.vault.on('rename', this.updateVaulter.bind(this)));
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    // This adds a simple command that can be triggered anywhere
+    this.addCommand({
+      id: 'show-vault-stats',
+      name: 'Show Vault Stats',
+      callback: () => {
+        new Notice(`Total Notes: ${this.getNoteCount()}`);
+      }
+    });
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    // This adds a settings tab so the user can configure various aspects of the plugin
+    this.addSettingTab(new VaulterSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+    // Using this function will automatically remove the event listener when this plugin is disabled.
+    this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+      console.log('click', evt);
+    });
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-		  
-	}
+    // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+    this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+  }
 
-	onunload() {
-	}
+  // Function to count markdown files (notes)
+  getNoteCount(): number {
+    return this.app.vault.getFiles().filter(file => file.extension === "md").length;
+  }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+  // Function to update the status bar with the note count
+  updateVaulter() {
+    if (this.settings.showInStatusBar) {
+      const totalNotes = this.getNoteCount();
+      this.statusBarItemEl.setText(`This vault has ${totalNotes} notes`);
+    } else {
+      this.statusBarItemEl.setText('');  // Clear status bar if setting is off
+    }
+  }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  onunload() {
+  }
+
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class VaulterSettingTab extends PluginSettingTab {
+  plugin: VaulterPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+  constructor(app: App, plugin: VaulterPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+  display(): void {
+    const { containerEl } = this;
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: VaultStatsPlugin;
+    containerEl.empty();
 
-	constructor(app: App, plugin: VaultStatsPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    new Setting(containerEl)
+      .setName('Show note count in status bar')
+      .setDesc('Toggle whether the note count is displayed in the status bar')
+      .addToggle(toggle => 
+        toggle
+          .setValue(this.plugin.settings.showInStatusBar)
+          .onChange(async (value) => {
+            this.plugin.settings.showInStatusBar = value;
+            await this.plugin.saveSettings();
+            this.plugin.updateVaulter();  // Update the status bar immediately
+          })
+      );
+  }
 }
